4c9c3d5b5460c9163e53e6eb4166a09065a58858
mkabtoul <kabtoul.maria@gmail.com>
2025-11-19 17:22:09 +0100

    (refactor) Moves robot plugin methods and classes to a new module

--- ros_sugar/io/supported_types.py
+++ ros_sugar/io/supported_types.py
@@ -1,9 +1,8 @@
 """ROS Topics Supported Message Types"""
 
-from typing import Any, Union, Optional, List, Dict, Callable
+from typing import Any, Union, Optional, List, Dict
 import base64
 import numpy as np
-import inspect
 
 # GEOMETRY MSGS SUPPORTED ROS TYPES
 from geometry_msgs.msg import Point as ROSPoint
@@ -160,7 +159,7 @@ class SupportedType:
     callback = callbacks.GenericCallback
 
     @classmethod
-    def convert(cls, output, **_) -> Any:
+    def convert(cls, *output, **_) -> Any:
         """ROS message converter function for datatype
         :param args:
         :type _: Any
@@ -180,80 +179,6 @@ class SupportedType:
     @classmethod
     def convert_ui_dict(cls, data: Dict, **_) -> str:
         return data.get("data", "")
-
-
-def create_supported_type(ros_msg_type: type, converter: Optional[Callable] = None, callback: Optional[Callable] = None) -> type[SupportedType]:
-    """Add a new SupportedType derived class to the existing class
-    """
-    if not hasattr(ros_msg_type, "SLOT_TYPES"):
-        raise TypeError("ros_msg_type must be a valid ROS2 message type")
-
-    # Dynamically create the new class
-    class_name = f"{ros_msg_type.__name__}"
-    class_bases = (SupportedType,)
-    # Create the new class.
-    class_attrs = {
-        "_ros_type": ros_msg_type,
-    }
-
-    if converter:
-        # Inspect the converter method to accept a python type and return the same ros_msg_type
-        converter_sig = inspect.signature(converter)
-        c_params = list(converter_sig.parameters.keys())
-
-        # Must have at least one input parameter
-        if len(c_params) < 1:
-            raise TypeError("Converter must take at least one parameter")
-
-        if converter_sig.return_annotation != ros_msg_type:
-            raise TypeError(
-                f"Converter return annotation ({converter_sig.return_annotation}) "
-                f"must match ros_msg_type ({ros_msg_type})"
-            )
-
-        def _convert(cls: object, **_):
-            return converter(**_)
-        class_attrs["convert"] = classmethod(_convert)
-
-    if callback:
-        # Inspect the callback to accept the ros_msg_type and return a known python type
-        callback_sig = inspect.signature(callback)
-        cb_params = list(callback_sig.parameters.values())
-
-        if not cb_params:
-            raise TypeError("Callback function must accept at least one argument.")
-
-        # Check that the first argument's type hint matches the ROS message type
-        if cb_params[0].annotation != ros_msg_type:
-            raise TypeError(
-                f"Callback's first argument annotation ({cb_params[0].annotation}) "
-                f"must match ros_msg_type ({ros_msg_type})"
-            )
-
-        # Check that the return type is a python type (not the ROS message itself)
-        if (
-            callback_sig.return_annotation == ros_msg_type
-            or callback_sig.return_annotation == inspect.Parameter.empty
-        ):
-            raise TypeError(
-                "Callback must have a return annotation of a non-ROS, Python type (e.g., str, int, Dict)."
-            )
-
-        def _get_output(self: object, **_):
-            return callback(self.msg, **_)
-
-        # Create callback class
-        callback_class_attrs = {
-            "_get_output": _get_output,  # Make the converter a classmethod
-        }
-        callback_class = type(
-            f"{class_name}Callback", (callbacks.GenericCallback,), callback_class_attrs
-        )
-        class_attrs["callback"] = callback_class
-
-    new_type = type(class_name, class_bases, class_attrs)
-    add_additional_datatypes([new_type])
-    return new_type
 
 
 class String(SupportedType):
