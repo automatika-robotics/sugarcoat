3c7bcfc2ef2dba622ab3e115eacf73868b0cf4b9
ahr <ahrasheed@gmail.com>
2026-02-18 17:47:39 +0100

    (docs) Fixes base url and sitemap

--- docs/conf.py
+++ docs/conf.py
@@ -66,17 +66,15 @@ myst_html_meta = {
 }
 myst_heading_anchors = 7  # to remove cross reference errors with md
 
-html_baseurl = "https://automatika-robotics.github.io/sugarcoat/"
+html_baseurl = "https://sugarcoat.automatikarobotics.com/"
 language = "en"
-
-
 html_theme = "shibuya"  # install with `pip install shibuya`
-
 html_static_path = ["_static"]
 html_css_files = [
     "custom.css",
 ]
 html_favicon = "_static/favicon.png"
+sitemap_url_scheme = "{link}"
 
 html_theme_options = {
     "light_logo": "_static/SUGARCOAT_LIGHT.png",
@@ -177,7 +175,7 @@ def create_robots_txt(app, exception):
         robots_path = os.path.join(dst_dir, "robots.txt")
         content = f"""User-agent: *
 
-Sitemap: {html_baseurl}/sitemap.xml
+Sitemap: {html_baseurl}sitemap.xml
 """
         with open(robots_path, "w") as f:
             f.write(content)
